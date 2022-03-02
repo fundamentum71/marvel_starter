a6259e64188ce8df8d2a01e8ebfcb85e15f139f8
Aleksandr <it.kucherov@gmail.com>
2022-03-02 16:24:29 +0300

    add CharInfo

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -19,7 +19,7 @@ class RandomChar extends Component {
 
 	componentDidMount() {
 		this.updateChar();
-		//this.timerId = setInterval(this.updateChar, 13000);
+		//this.timerId = setInterval(this.updateChar, 1500);
 	}
 	componentWillUnmount() {
 		clearInterval(this.timerId);
@@ -44,6 +44,7 @@ class RandomChar extends Component {
 
 	updateChar = () => {
 		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+		this.onCharLoadeding();
 		this.marvelService.getCharacter(id).then(this.onCharLoaded).catch(this.onError);
 	};
 
